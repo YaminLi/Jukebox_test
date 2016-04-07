ffe7168f27a765fadedaca04bdb4c14ffcada9e8
YaminLi <liyamin0222@gmail.com>
2016-04-06 23:40:41 -0500

    Update song.go

--- routes/song.go
+++ routes/song.go
@@ -10,9 +10,13 @@ import (
 
 )
 
+var idAssignmentChan = make(chan string)
+
 func addSongRoutes(r *mux.Router) {
   r.HandleFunc("/songs", getSongsHandler).Methods("GET")
   r.HandleFunc("/songs", createSongHandler).Methods("POST")
+  r.HandleFunc("/songs/{song_id}/upvote", upvoteSongHandler).Methods("POST") // TODO
+  r.HandleFunc("/songs/{song_id}", deleteSongHandler).Methods("DELETE") // TODO
 }
 
 func getSongsHandler(w http.ResponseWriter, req *http.Request) {
@@ -39,9 +43,74 @@ func createSongHandler(w http.ResponseWriter, req *http.Request) {
     return
   }
 
+  //Initialize Id
+  go func ()  {
+    var i uint64
+    for i = 0;  ; i++ {
+        idAssignmentChan <- strconv.FormatUint(i, 10)
+    }
+  }()
+
+  song_id := <- idAssignmentChan
+  song.Id = song_id
+
+  // Initialize votes value
+  song.Votes = 1
+
+
   // Save song
   models.DB.Create(&song)
 
   // Create response
+  json.NewEncoder(w).Encode(&song)
+}
+
+// TODO
+func deleteSongHandler(w http.ResponseWriter, req *http.Request) {
+  song_id := mux.Vars(req)["song_id"]
+  fmt.Println(song_id)
+
+  // Get song by id
+  var songs []models.Song
+  models.DB.Find(&songs)
+  var song Song
+  for elem := range songs{
+    if elem.id == song_id{
+        song = elem
+        break
+    }
+  }
+
+  // Delete song
+  models.DB.Delete(&song)   //????????????
+
+  // Return response
+  json.NewEncoder(w).Encode(&song)
+}
+
+// TODO
+func upvoteSongHandler(w http.ResponseWriter, req *http.Request) {
+  song_id := mux.Vars(req)["song_id"]
+  fmt.Println(song_id)
+
+  // Get song by id specified
+  var songs []models.Song
+  models.DB.Find(&songs)
+  var song Song
+  for elem := range songs{
+    if elem.id == song_id{
+        song = elem
+        break
+    }
+  }
+
+  // +1 to score
+  song.Votes += 1
+
+  // Save song
+  models.DB.Save(&song)
+  // models.DB.Update(&song) //?????????
+
+  // Return song in response
   json.NewEncoder(w).Encode(&song)
 }
